9fdfec269ae86f2608c17d6edc38068418dd96d8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-20 00:18:57 +0900

    feat: add paths.ts

---
+++ src/paths.ts
@@ -0,0 +1,25 @@
+type Path = {
+  path: string;
+  name: string;
+};
+
+const paths: Path[] = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/about',
+    name: 'About me',
+  },
+  {
+    path: '/posts',
+    name: 'Posts',
+  },
+  {
+    path: '/apps',
+    name: 'Apps',
+  },
+];
+
+export default paths;
