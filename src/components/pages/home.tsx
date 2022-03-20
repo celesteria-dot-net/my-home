9e65363a315cea23af5e8c1644a81d4d4b484277
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-20 21:22:33 +0900

    fix: remove the link to top from home

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import { externalPaths, internalIcons } from '@/paths';
+import { externalPaths, internalIcons, internalPaths } from '@/paths';
 
 const Home = () => (
   <div className="hero min-h-screen max-h-screen">
@@ -9,16 +9,16 @@ const Home = () => (
       <h1 className="text-4xl font-bold">Lucky (Lucky3028)</h1>
       <div className="divider" />
       <ul className="menu menu-vertical md:menu-horizontal rounded-box text-xl">
-        {Object.values(internalIcons({ className: 'w-8', fill: 'white' })).map(
-          ({ path, icon, title }) => (
+        {Object.values(internalIcons({ className: 'w-8', fill: 'white' }))
+          .filter(({ path }) => path !== internalPaths.home.path)
+          .map(({ path, icon, title }) => (
             <li key={path}>
               <Link to={path}>
                 {icon}
                 {title}
               </Link>
             </li>
-          ),
-        )}
+          ))}
       </ul>
       <div className="divider" />
       <div className="btn-group">
