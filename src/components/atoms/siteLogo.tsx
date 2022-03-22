fbfc0273f701478d2aeff766f410b1b57598a791
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-23 00:29:10 +0900

    feat: add logo component

---
+++ src/components/atoms/siteLogo.tsx
@@ -0,0 +1,10 @@
+const SiteLogo = () => (
+  <h1
+    className="btn btn-ghost normal-case text-2xl"
+    style={{ fontFamily: 'Duma' }}
+  >
+    celesteria.net
+  </h1>
+);
+
+export default SiteLogo;
