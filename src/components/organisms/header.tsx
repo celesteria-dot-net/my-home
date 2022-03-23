330ea937e0c22cdc83763542da28d803c5e5c106
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-23 15:05:01 +0900

    feat(header): add link to home in header logo

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import SiteLogo from '@/components/atoms/siteLogo';
 import NavBar from '@/containers/molecules/navBar';
+import { internalPaths } from '@/paths';
 
 const Header = () => (
   <div className="navbar bg-blue-300 flex-col md:flex-row">
     <div className="flex-1">
-      <SiteLogo />
+      <Link to={internalPaths.home.path}>
+        <SiteLogo />
+      </Link>
     </div>
     <div className="flex-none">
       <NavBar className="w-5" fill="black" />
