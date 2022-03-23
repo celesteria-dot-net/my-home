f8c50f8a45a5ec8a155b2dcfa2e460da173e9e62
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-23 15:26:44 +0900

    fix(container): adjust margin and padding

--- src/components/atoms/container.tsx
+++ src/components/atoms/container.tsx
@@ -6,7 +6,7 @@ const Container = ({
     <div
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
-      className="container mt-3 p-3 px-4 md:px-20 min-h-min min-w-min"
+      className="container my-3 p-3 mx-20 md:mx-40 min-h-min min-w-min"
     >
       {children}
     </div>
