564c3f86879873f614a97de6f4a8096fee59a55b
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-23 12:17:41 +0900

    refactor: move font config
    use tailwind config

--- src/components/atoms/siteLogo.tsx
+++ src/components/atoms/siteLogo.tsx
@@ -1,8 +1,5 @@
 const SiteLogo = () => (
-  <h1
-    className="btn btn-ghost normal-case text-2xl"
-    style={{ fontFamily: 'Duma' }}
-  >
+  <h1 className="btn btn-ghost normal-case text-2xl font-duma">
     celesteria.net
   </h1>
 );
