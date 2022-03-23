5b7e70794f5a90f3e814046fcff53183f96d0bd8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-23 14:52:51 +0900

    fix: container must be center

--- src/components/atoms/container.tsx
+++ src/components/atoms/container.tsx
@@ -2,12 +2,14 @@ const Container = ({
   children,
   ...props
 }: JSX.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    {...props}
-    className="container p-3 px-7 mt-3 md:px-20 min-h-min min-w-min"
-  >
-    {children}
+  <div className="flex justify-center">
+    <div
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className="container mt-3 p-3 px-4 md:px-20 min-h-min min-w-min"
+    >
+      {children}
+    </div>
   </div>
 );
 
