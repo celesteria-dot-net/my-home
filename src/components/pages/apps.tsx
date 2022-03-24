5cc051af1e61b1c0054d7a1c3a7391ed9b418a8f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-24 17:10:58 +0900

    fix(apps): translate error msg to ja

--- src/components/pages/apps.tsx
+++ src/components/pages/apps.tsx
@@ -45,7 +45,7 @@ const Apps = ({ applications = [] }: Prop) => (
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <span>There are no applications. Under development.</span>
+            <span>現在公開中のアプリケーションはありません。開発中です。</span>
           </div>
         </div>
       )}
