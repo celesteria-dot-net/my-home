23598d0e314aa10b16a6e02cf4bd502d264c62d0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-24 20:32:41 +0900

    refactor: optimize imports

--- src/components/pages/apps.tsx
+++ src/components/pages/apps.tsx
@@ -1,6 +1,4 @@
-import { Application } from '@/applications';
 import Container from '@/components/atoms/Container';
-import ExLink from '@/components/atoms/ExLink';
 import Heading2 from '@/components/atoms/headings';
 import Header from '@/components/organisms/Header';
 import ApplicationsList from '@/containers/organisms/ApplicationsList';
