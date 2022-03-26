6e031575d032c437744ca69d7105a180d1aaae05
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-27 00:23:15 +0900

    feat: add postlist page

--- src/components/pages/posts.tsx
+++ src/components/pages/posts.tsx
@@ -1,3 +1,17 @@
-const Posts = () => <p>posts</p>;
+import Container from '@/components/atoms/Container';
+import Heading2 from '@/components/atoms/headings';
+import Header from '@/components/organisms/Header';
+import PostList from '@/containers/molecules/PostList';
+import { internalPaths } from '@/paths';
+
+const Posts = () => (
+  <>
+    <Header />
+    <Container>
+      <Heading2>{internalPaths.posts.title}</Heading2>
+      <PostList />
+    </Container>
+  </>
+);
 
 export default Posts;
