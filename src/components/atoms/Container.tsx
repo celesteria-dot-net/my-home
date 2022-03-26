e3ae84006a4a1873c8f57ac301eb6008e6dc9ee2
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-27 00:19:34 +0900

    refactor(container): 余白を狭く

--- src/components/atoms/Container.tsx
+++ src/components/atoms/Container.tsx
@@ -6,7 +6,7 @@ const Container = ({
     <div
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
-      className="container my-3 p-3 mx-20 md:mx-40 min-h-min min-w-min"
+      className="container my-3 p-3 mx-8 md:mx-40 min-h-min min-w-min w-full"
     >
       {children}
     </div>
