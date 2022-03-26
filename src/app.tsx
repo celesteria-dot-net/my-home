cd62cdb87930728b2461b651abc804bdb759708a
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-27 00:23:42 +0900

    feat: add routes for posts

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from '@/components/pages/404';
 import About from '@/components/pages/about';
 import Apps from '@/components/pages/apps';
+import Post from '@/components/pages/post';
 import Posts from '@/components/pages/posts';
 import Home from '@/containers/pages/home';
 import { internalPaths } from '@/paths';
@@ -12,7 +13,10 @@ const App = () => (
     <Routes>
       <Route index element={<Home />} />
       <Route path={internalPaths.about.path} element={<About />} />
-      <Route path={internalPaths.posts.path} element={<Posts />} />
+      <Route path={internalPaths.posts.path}>
+        <Route index element={<Posts />} />
+        <Route path=":postId" element={<Post />} />
+      </Route>
       <Route path={internalPaths.apps.path} element={<Apps />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
